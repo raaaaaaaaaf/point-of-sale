d74389a1af561d4f3e9b4c0146e4c8ed93ad4e60
raaaaaaaaaf <rridad@ssct.edu.ph>
2023-11-15 22:43:55 +0800

    Dashboard Done

--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -1,49 +1,25 @@
-import { faker } from '@faker-js/faker';
-
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
-
-import Iconify from 'src/components/iconify';
-
-import AppTasks from '../app-tasks';
-import AppNewsUpdate from '../app-news-update';
-import AppOrderTimeline from '../app-order-timeline';
 import AppCurrentVisits from '../app-current-visits';
 import AppWebsiteVisits from '../app-website-visits';
 import AppWidgetSummary from '../app-widget-summary';
-import AppTrafficBySite from '../app-traffic-by-site';
-import AppCurrentSubject from '../app-current-subject';
-import AppConversionRates from '../app-conversion-rates';
 import { useEffect, useState } from 'react';
-import { collection, getDocs, query } from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from 'src/firebase/firebaseConfig';
+import { fCurrency } from 'src/utils/format-number';
 
 // ----------------------------------------------------------------------
 
 export default function AppView() {
-  const [labels, setLabels] = useState([]);
   const [sales, setSales] = useState([]);
-
-  useEffect(() => {
-    const getLast7Days = () => {
-      const today = new Date();
-      const newLabels = [];
-
-      for (let i = 6; i >= 0; i--) {
-        const currentDate = new Date(today);
-        currentDate.setDate(today.getDate() - i);
-
-        // Format the date as 'MM/DD/YYYY'
-        const formattedDate = `${
-          currentDate.getMonth() + 1
-        }/${currentDate.getDate()}/${currentDate.getFullYear()}`;
-        newLabels.push(formattedDate);
-      }
-      return newLabels;
-    };
-    setLabels(getLast7Days());
-  }, []);
+  const [todayOrders, setTodayOrders] = useState('');
+  const [totalOrders, setTotalOrders] = useState('');
+  const [totalSales, setTotalSales] = useState('');
+  const [today, setToday] = useState('');
+  const [yesterday, setYesterday] = useState('');
+  const [threeDays, setThreeDays] = useState('');
+  const [sevenDays, setSevenDays] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -57,70 +33,152 @@ export default function AppView() {
             ...doc.data(),
           });
         });
+
+        const today = new Date();
+        const todays = new Date(new Date().setDate(today.getDate()));
+        const yesterday = new Date(new Date().setDate(today.getDate() - 1));
+        const threeDaysAgo = new Date(new Date().setDate(today.getDate() - 3));
+        const fourDaysAgo = new Date(new Date().setDate(today.getDate() - 4));
+        const sevenDaysAgo = new Date(new Date().setDate(today.getDate() - 7));
+
+        const todayRef = query(
+          collection(db, 'data_sales'),
+          where('timeStamp', '>', yesterday),
+          where('timeStamp', '<=', todays)
+        );
+        const yesterdayRef = query(
+          collection(db, 'data_sales'),
+          where('timeStamp', '>', threeDaysAgo),
+          where('timeStamp', '<=', yesterday)
+        );
+        const threeDaysAgoRef = query(
+          collection(db, 'data_sales'),
+          where('timeStamp', '>', fourDaysAgo),
+          where('timeStamp', '<=', threeDaysAgo)
+        );
+        const sevenDaysAgoRef = query(
+          collection(db, 'data_sales'),
+          where('timeStamp', '>', sevenDaysAgo),
+          where('timeStamp', '<=', fourDaysAgo)
+        );
+
+        const todaySnap = await getDocs(todayRef);
+        const yesterdaySnap = await getDocs(yesterdayRef);
+        const threeDaysSnap = await getDocs(threeDaysAgoRef);
+        const sevenDaysSnap = await getDocs(sevenDaysAgoRef);
+
+        const todayDataArray = todaySnap.docs.map((doc) => doc.data());
+        const todayTotal = todayDataArray.reduce((acc, item) => acc + item.totalPrice, 0);
+
+        const yesterdayDataArray = yesterdaySnap.docs.map((doc) => doc.data());
+        const yesterdayTotal = yesterdayDataArray.reduce((acc, item) => acc + item.totalPrice, 0);
+
+        const threeDaysDataArray = threeDaysSnap.docs.map((doc) => doc.data());
+        const threeDaysTotal = threeDaysDataArray.reduce((acc, item) => acc + item.totalPrice, 0);
+
+        const sevenDaysDataArray = sevenDaysSnap.docs.map((doc) => doc.data());
+        const sevenDaysTotal = sevenDaysDataArray.reduce((acc, item) => acc + item.totalPrice, 0);
+
+        const totalSales = data.reduce((acc, item) => acc + item.totalPrice, 0);
+
+        setToday(todayTotal);
+        setYesterday(yesterdayTotal);
+        setThreeDays(threeDaysTotal);
+        setSevenDays(sevenDaysTotal);
+
+        setTotalOrders(dataSnap.docs.length);
+        setTodayOrders(todaySnap.docs.length);
+        setTotalSales(totalSales);
         setSales(data);
       } catch (err) {
         console.error(err);
       }
     };
-    fetchData()
+    fetchData();
   }, []);
+  const items = sales.map((entry) => entry.items).flat();
 
-  console.log(sales)
+  // Creating a map to accumulate quantities for each product
+  const productQuantitiesMap = items.reduce((acc, item) => {
+    const productName = item.productName;
+    const quantity = parseInt(item.quantity, 10);
+
+    if (acc.has(productName)) {
+      acc.set(productName, acc.get(productName) + quantity);
+    } else {
+      acc.set(productName, quantity);
+    }
+
+    return acc;
+  }, new Map());
+
+  // Creating the series data for the chart
+  const seriesData = Array.from(productQuantitiesMap.entries()).map(([label, value]) => ({
+    label,
+    value,
+  }));
+
+  console.log(sales);
   return (
     <Container maxWidth="xl">
       <Typography variant="h4" sx={{ mb: 5 }}>
-        Hello Image Upload, Raff👋
+      POS of B & M Store
       </Typography>
 
       <Grid container spacing={3}>
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
             title="Today's Orders"
-            total={714000}
+            total={todayOrders}
             color="success"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
+            icon={<img alt="icon" src="/assets/icon/briefcase.png" />}
           />
         </Grid>
 
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
             title="Total Orders"
-            total={1352831}
+            total={totalOrders}
             color="info"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
+            icon={<img alt="icon" src="/assets/icon/strategic-plan.png" />}
           />
         </Grid>
 
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
             title="Today's Sales"
-            total={1723315}
+            total={`₱ ${fCurrency(today)}`}
             color="warning"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
+            icon={<img alt="icon" src="/assets/icon/coins.png" />}
           />
         </Grid>
 
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
             title="Total Sales"
-            total={234}
+            total={`₱ ${fCurrency(totalSales)}`}
             color="error"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
+            icon={<img alt="icon" src="/assets/icon/money.png" />}
           />
         </Grid>
 
         <Grid xs={12} md={6} lg={8}>
           <AppWebsiteVisits
-            title="Sales by Date"
-            subheader="(+43%) than last year"
+            title="Last 7 Days Sales"
+            subheader=""
             chart={{
-              labels: labels,
+              labels: [
+                'Today',
+                'Yesterday',
+                'Last 3 Days',
+                'Last 4-7 Days', // A range that includes the last 7 days
+              ],
               series: [
                 {
                   name: 'Sales',
                   type: 'column',
                   fill: 'solid',
-                  data: sales.map((entry) => entry.totalPrice),
+                  data: [`${today}`, `${yesterday}`, `${threeDays}`, `${sevenDays}`],
                 },
               ],
             }}
@@ -129,17 +187,13 @@ export default function AppView() {
 
         <Grid xs={12} md={6} lg={4}>
           <AppCurrentVisits
-            title="Current Visits"
+            title="Sale by Products"
             chart={{
-              series: [
-                { label: 'America', value: 9 },
-                { label: 'Asia', value: 6 },
-                { label: 'Europe', value: 4 },
-                { label: 'Africa', value: 3 },
-              ],
+              series: seriesData,
             }}
           />
         </Grid>
+
       </Grid>
     </Container>
   );
